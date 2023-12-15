c13dd505ea4d90b10f3c74afac5d808ea2ef41c1
EllaKaye <ella.kaye@gmail.com>
2023-12-15 21:52:05 +0000

    Speed run complete!

--- 5784/speedrun/speedrun.R
+++ 5784/speedrun/speedrun.R
@@ -27,13 +27,15 @@ candle1 <- function(customers) {
 investigator <- candle1(customers)
 investigator
 
+# This ran fine
+
 # Candle 2 ----------------------------------------------------------------
 
 candle2 <- function(customers, orders, orders_items, products) {
 	customers |> 
 		separate_wider_delim(name, delim = " ", names = c("first", "last"), too_many = "merge") |> 
-		filter(str_detect(first, "^J")) |> 
-		filter(str_detect(last, "^P")) |> 
+		filter(str_detect(first, "^D")) |> 
+		filter(str_detect(last, "^S")) |> 
 		left_join(orders, by = "customerid") |> 
 		filter(str_detect(ordered, "2017")) |> 
 		left_join(orders_items, by = "orderid") |> 
@@ -42,11 +44,15 @@ candle2 <- function(customers, orders, orders_items, products) {
 		group_by(customerid, phone, orderid) |> 
 		filter(any(str_detect(desc, "Coffee")) & any(str_detect(desc, "Bagel"))) |> 
 		distinct(customerid) |> 
-		pull(phone)
+		pull(phone) |> 
+		I()
 }
 
 contractor <- candle2(customers, orders, orders_items, products)
 contractor
+
+# Needed to change the initials 
+# (this caught me out as I assumed the puzzle text was exactly the same)
 
 # Candle 3 ----------------------------------------------------------------
 
@@ -57,8 +63,8 @@ candle3 <- function(customers, contractor) {
 		pull(citystatezip)
 	
 	customers |> 
-		filter(year(birthdate) %in% c(1939, 1951, 1963, 1975, 1987, 1999, 2011)) |> 
-		filter(month(birthdate) == 6 & day(birthdate) >= 21 | month(birthdate) == 7 & day(birthdate) <= 22) |> 
+		filter(year(birthdate) %in% c(1931, 1943, 1955, 1967, 1979, 1991, 2003, 2015)) |> 
+		filter(month(birthdate) == 9 & day(birthdate) >= 21 | month(birthdate) == 10 & day(birthdate) <= 22) |> 
 		filter(citystatezip == neighborhood) |> 
 		pull(phone)
 	
@@ -67,7 +73,11 @@ candle3 <- function(customers, contractor) {
 neighbor <- candle3(customers, contractor)
 neighbor
 
+# Needed to change the astrological details (goat/libra instead rabbit/cancer)
+
 # Candle 4 ----------------------------------------------------------------
+
+# This ran fine
 
 candle4 <- function(customers, orders, orders_items) {
 	orders |> 
@@ -90,7 +100,7 @@ early_bird
 
 candle5 <- function(customers, orders, orders_items) {
 	customers |> 
-		filter(str_detect(citystatezip, "Staten")) |> 
+		#filter(str_detect(citystatezip, "Staten")) |> 
 		left_join(orders, by = "customerid") |> 
 		left_join(orders_items, by = "orderid") |> 
 		left_join(products, by = "sku") |> 
@@ -103,6 +113,9 @@ candle5 <- function(customers, orders, orders_items) {
 }
 cat_lady <- candle5(customers, orders, orders_items)
 cat_lady
+
+# No mention this time of Staten Island, 
+# so needed to comment that out to get the correct solution
 
 # Candle 6 ----------------------------------------------------------------
 
@@ -121,6 +134,8 @@ candle6 <- function(customers, orders, orders_items, products) {
 }
 bargain_hunter <- candle6(customers, orders, orders_items, products)
 bargain_hunter
+
+# This ran fine
 
 # Candle 7 ----------------------------------------------------------------
 
@@ -152,17 +167,26 @@ candle7 <- function(customers, orders, orders_items, products, bargain_hunter) {
 		left_join(products, by = "sku") |> 
 		select(-qty, -unit_price, -wholesale_cost, -dims_cm) 
 	
+	# Make regex for colours
+	colours <- products |> 
+		filter(str_detect(desc, "Poster")) |>  # 12 colours, all with COL in sku
+		arrange(desc) |> 
+		separate_wider_regex(desc, c(item = ".*", " ", colour = ".*")) |> 
+		pull(colour) |> 
+		paste(collapse = "|") |> 
+		str_remove_all("\\(|\\)") 
+	
 	# Orders of items with a colour on the dates that the bargain hunter shopped
 	# Where two or more people bought the same item on that day
 	# And where one of the two was the bargain hunter
 	same_products <- same_date_orders |> 
-		filter(str_detect(sku, "COL")) |> 
+		filter(str_detect(desc, colours)) |> 
 		separate_wider_regex(desc, c(item = ".*", " ", colour = ".*")) |> 
-		group_by(shipped_date) |> 
-		add_count(item) |> 
-		filter(n > 1) |> 
-		mutate(has_bargain_hunter = any(is_bargain_hunter)) |> 
-		filter(has_bargain_hunter) |> 
+		group_by(shipped_date) |>
+		add_count(item) |>
+		filter(n > 1) |>
+		mutate(has_bargain_hunter = any(is_bargain_hunter)) |>
+		filter(has_bargain_hunter) |>
 		ungroup()
 	
 	# separate in bargain hunter and possible meet cute
@@ -179,14 +203,20 @@ candle7 <- function(customers, orders, orders_items, products, bargain_hunter) {
 	# find for row where colour is different and time is closest
 	# that's the meet cute, so join with customers and pull phone
 	inner_join(bh, possible_meet_cute, by = c("shipped_date", "item")) |> 
-		filter(colour.x != colour.y) |> 
-		mutate(time_diff = abs(shipped.x - shipped.y)) |> 
-		slice_min(time_diff) |> 
-		left_join(customers, join_by(customerid.y == customerid)) |> 
+		filter(colour.x != colour.y) |>
+		mutate(time_diff = abs(shipped.x - shipped.y)) |>
+		slice_min(time_diff) |>
+		left_join(customers, join_by(customerid.y == customerid)) |>
 		pull(phone)
 }
 meet_cute <- candle7(customers, orders, orders_items, products, bargain_hunter) 
 meet_cute
+
+# There was a bug due to an assumption I'd made.
+# I used COL to filter for items with colours, but these are COLlector items (hinted at in puzzle 8), 
+# and I should have paid more attention to my own original working notes about this
+# and done what I'd originally said I'd come back to, getting colours from a product!
+# Had to add that to the function
 
 # Candle 8 ----------------------------------------------------------------
 
@@ -204,14 +234,15 @@ candle8 <- function(customers, orders, orders_items, products) {
 collector <- candle8(customers, orders, orders_items, products)
 collector	
 
+# This ran fine
 
 # All answers -------------------------------------------------------------
 
-cat("Investigator:", investigator)
-cat("Contractor:", contractor)
-cat("Neighbor:", neighbor)
-cat("Early Bird:", early_bird)
-cat("Cat Lady:", cat_lady)
-cat("Bargain Hunter:", bargain_hunter)
-cat("Meet Cute:", meet_cute)
-cat("Collector:", collector)
+cat("Investigator:", investigator) # 767-365-7269
+cat("Contractor:", contractor) # 838-351-0370
+cat("Neighbor:", neighbor) # 914-594-5535
+cat("Early Bird:", early_bird) # 716-789-4433
+cat("Cat Lady:", cat_lady) # 347-835-2358
+cat("Bargain Hunter:", bargain_hunter) # 838-295-7143
+cat("Meet Cute:", meet_cute) # 516-544-4187
+cat("Collector:", collector) # 516-638-9966
